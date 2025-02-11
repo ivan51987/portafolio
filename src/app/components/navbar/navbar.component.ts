1cce0f77f01109a70e8b2369ca8fff51781dbf08
talivan5 <talivan51987@gmail.com>
2025-02-11 09:44:54 -0400

    subiendo los ultimos cambios del footer

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuOpen = false;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    console.log("Menú presionado:", this.menuOpen);
+    // Actualiza la vista
+    this.cdr.detectChanges(); // Si es necesario
+  }
 }
